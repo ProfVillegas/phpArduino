2cd11225f84464f2b7d2bc3f963c60aa5a205c3d
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2019-03-02 13:35:44 -0500

    Update ejemplo2.php
    metodo post

--- sitio1/ejemplo2.php
+++ sitio1/ejemplo2.php
@@ -13,7 +13,20 @@
         <label>Correo Electronico<input type="email" name="correo" id="correo" /></label>
         <input type="submit" value="Enviar" name="enviar" />    
     </form>
-    
+    <?php
+        if($_GET){
+            print_r($_GET);
+        }
+    ?>
+    <form method="post" action="ejemplo2.php" enctype="application/x-www-form-urlencoded">
+        <label>Correo Electronico<input type="email" name="correo" id="correo" /></label>
+        <input type="hidden" name="section" value="45" />
+        <input type="submit" value="Enviar" name="enviar" />    
+    </form>
+        if($_POST){
+            print_r($_POST);
+        }
+    ?>
 </body>
 
 </html>
